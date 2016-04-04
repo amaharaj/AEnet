fd0a566d4894374146b62298ffcd0f62dafddb64
amaharaj <amber.maharaj@uoit.net>
2016-04-04 15:39:42 -0400

    script to link all xsf files + header to make input file: generate.in

---
+++ run/01-generate/list2file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm file
+counter=0
+# read all contents of directory into a single file
+for entry in `ls ../xsf/`; do
+   echo ../xsf/$entry >> file
+   counter=$((counter + 1))
+done
+
+# add header to beginning of file
+echo $counter | cat - file | sponge file
+echo "FILES" | cat - file | sponge file
