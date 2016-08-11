fce9b3ee05007bd5b70a631c162ccc414e774ca1
amaharaj <amber.maharaj@uoit.net>
2016-08-11 19:58:49 -0400

    Update randomSample.py

--- Test_Set/randomSample.py
+++ Test_Set/randomSample.py
@@ -3,11 +3,14 @@ import random
 import sys
 
 # execute as: python randomSample.py <number of samples>
+# randomly selects structure files 
+# currently needs manual input for number of structure files (to be fixed)
 
 samples = sys.argv[1]
 samples = int(samples)
+nfiles = 19815
 
 for i in range(samples):
-   a = random.randint(1,19815)
+   a = random.randint(1,nfiles)
    n = '%06d' % a
    os.system("cp ../structure{0}.xsf .".format(n) )
