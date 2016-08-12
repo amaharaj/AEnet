1f470243849b10575ced815caf2ce11e8fd5500b
amaharaj <amber.maharaj@uoit.net>
2016-08-11 20:07:36 -0400

    Update list2file.sh

--- run/01-generate/list2file.sh
+++ run/01-generate/list2file.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# script to generate footer to the generate.in file
+# run this script and then `cat header file > generate.in`
+
 rm file
 counter=0
 # read all contents of directory into a single file
